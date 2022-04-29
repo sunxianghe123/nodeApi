3e878d4c3be28e8bc9b87a53b5697973728d25c3
sunxianghe123 <924909613@qq.com>
2022-04-29 16:49:24 +0800

    feat: 用户编辑接口

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -89,21 +89,11 @@ let getUserList = async (req, res, next) => {
  */
 let getUser = (username, callback) => {
   callback = callback || null;
-  let sql = `select * from user where id=? or phone=? or username=?`;
+  let sql = `select * from user_info where user_id=? or phone=? or username=?`;
   let sqlArr = [username, username, username];
   return dbConfig.SySqlConnect(sql, sqlArr, callback);
 }
 
-/**
- * 获取用户表中数据条数（已完成）
- * @param table_name
- * @returns {Promise<unknown | void>}
- */
-let getUserCount = (table_name) => {
-  let sql = `select count(*) from ${table_name}`;
-  let sqlArr = [];
-  return dbConfig.SySqlConnect(sql, sqlArr);
-}
 
 /**
  * 用户注册接口（已完成）
@@ -112,20 +102,15 @@ let getUserCount = (table_name) => {
  * @returns {Promise<void>}
  */
 let postRegister = async (req, res) => {
-  console.log(req.body);
   let {username, avatar, password, phone, email} = req.body;
   // 检测用户是否存在
-  // let avatar = 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fblog%2F202103%2F18%2F20210318191428_03a54.jpg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652859044&t=35c7a7402684b3fa0db4d33040c13f67';
-  let create_time = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+  let created_at = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
   let user = await getUser(username);
-  let count = (await getUserCount('user'))[0]['count(*)'];
-  let id = count + 1;
-  // console.log(user, 'user');
-  // console.log(count, 'count');
-  // let sql = null, sqlArr = null, callback = null;
+  let count = (await getCount('user_info'))[0]['count(*)'];
+  let user_id = count + 1;
   if (!user[0]) {
-    let sql = `insert into user(id, username, avatar, password, phone, email, create_time) value(?,?,?,?,?,?,?)`;
-    let sqlArr = [id, username, avatar, password, phone, email, create_time];
+    let sql = `insert into user_info(user_id, username, avatar, password, phone, email, created_at) value(?,?,?,?,?,?,?)`;
+    let sqlArr = [user_id, username, avatar, password, phone, email, created_at];
     let callback = (err, data) => {
       if (err) {
         console.log(err);
@@ -160,10 +145,11 @@ let postRegister = async (req, res) => {
  * @param res
  */
 let getCurrentUserInfo = (req, res) => {
-  let {username} = req.query;
-  let sql = `select username,avatar,age,sex,job,path,birthday,email,phone,is_locked,created_at from user_info where username=?`;
-  let sqlArr = [username];
+  let {username, user_id} = req.query;
+  let sql = `select username,avatar,age,sex,job,path,birthday,email,phone,is_locked,created_at from user_info where username=? or user_id=?`;
+  let sqlArr = [username, user_id];
   let callback = (err, data) => {
+    // console.log(data)
     if (err) {
       console.log(err);
       res.send({
@@ -226,6 +212,7 @@ let insertUserInfo = (req, res) => {
   dbConfig.sqlConnect(sql, sqlArr, callback);
 }
 
+
 /**
  * 查看用户是否有详情信息
  * @param user_id
@@ -240,31 +227,21 @@ let findUserInfo = async (user_id) => {
 
 /**
  * 修改用户详细信息
- * @param user_id
- * @param username
- * @param age
- * @param sex
- * @param job
- * @param path
- * @param birthday
- * @returns {Promise<*>}
  */
-let setUserInfo = async (user_id, username, age, sex, job, path, birthday) => {
-  console.log(user_id, username, age, sex, job, path, birthday);
+let setUserInfo = async (user_id, username, password, avatar, age, sex, job, path, birthday, email, phone) => {
   let hasUserInfo = await findUserInfo(user_id);
-  let sqlArr = [username, age, sex, job, path, birthday, user_id];
-  console.log(sqlArr, 'sqlArr');
+  let sqlArr = [username, password, avatar, age, sex, job, path, birthday, email, phone, user_id];
   if (hasUserInfo) {
-    let sql = `update user_info set username=?, age=?,sex=?,job=?,path=?,birthday=? where user_id=? `;
+    let sql = `update user_info set username=?,password=?,avatar=?,age=?,sex=?,job=?,path=?,birthday=?,email=? where user_id=? `;
     let res = await dbConfig.SySqlConnect(sql, sqlArr);
-    if (res.affectedRows == 1) {
+    if (res.affectedRows == 0) {
       let user = await getUser(user_id);
       let user_info = await getUserInfo(user_id);
       user[0].user_info = user_info[0];
       return user;
     }
   } else {
-    let sql = `insert into user_info (user_id,username,age,sex,job,path,birthday) values(?,?,?,?,?,?,?)`;
+    let sql = `insert into user_info (user_id, username, password, avatar, age, sex, job, path, birthday, email, phone) values(?,?,?,?,?,?,?,?,?,?,?)`;
     // let sqlArr = [user_id, age, sex, job, path, birthday];
     let res = await dbConfig.SySqlConnect(sql, sqlArr);
     if (res.affectedRows == 1) {
@@ -277,139 +254,19 @@ let setUserInfo = async (user_id, username, age, sex, job, path, birthday) => {
 }
 
 /**
- * 修改用户名称
- * @param user_id
- * @param username
- * @returns {Promise<boolean>}
- */
-// mysql_affected_rows() 函数返回前一次 MySQL 操作所影响的记录行数。
-let setUserName = async (user_id, username) => {
-  let sql = `update user set username=? where id=?`;
-  let sqlArr = [username, user_id];
-  let res = await dbConfig.SySqlConnect(sql, sqlArr);
-  return res.affectedRows == 1;
-}
-
-/**
- * 登录
- * @param req
- * @param res
- */
-//用户名、手机号登录,手机号+密码，邮箱+密码，用户名+密码
-let login = (req, res) => {
-  let username = req.body.username,
-      password = req.body.password;
-  let phone = /^1\d{10}$/;
-  let email = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
-  if (phone.test(username)) {
-    let sql = 'select * from user where phone=? and password=? or username=? and password=?';
-    let sqlArr = [username, password, username, password];
-    let callBack = async (err, data) => {
-      console.log(data);
-      if (err) {
-        console.log(err);
-        res.send({
-          code: 400,
-          msg: '出错了'
-        });
-      } else if (data == '') {
-        res.send({
-          code: 400,
-          msg: '用户名或者密码出错！'
-        });
-      } else {
-        let user_id = data[0].id;
-        let result = await getUserInfo(user_id);
-        data[0].user_info = result[0];
-        res.send({
-          code: 200,
-          msg: '登录成功',
-          data: data[0]
-        });
-      }
-    }
-    dbConfig.sqlConnect(sql, sqlArr, callBack);
-  } else if (email.test(username)) {
-    let sql = `select * from user where email=? and password=?`;
-    let sqlArr = [username, password];
-    let callBack = async (err, data) => {
-      if (err) {
-        console.log(err);
-        res.send({
-          code: 400,
-          msg: '出错了'
-        });
-      } else if (data == '') {
-        res.send({
-          code: 400,
-          msg: '用户名或者密码出错！'
-        });
-      } else {
-        let user_id = data[0].id;
-        let result = await getUserInfo(user_id);
-        data[0].user_info = result[0];
-        res.send({
-          code: 200,
-          msg: '登录成功',
-          data: data[0]
-        });
-      }
-    }
-    dbConfig.sqlConnect(sql, sqlArr, callBack);
-  } else {
-    let sql = `select * from user where username=? and password=?`;
-    let sqlArr = [username, password];
-    let callBack = async (err, data) => {
-      if (err) {
-        console.log(err);
-        res.send({
-          code: 400,
-          msg: '出错了'
-        });
-      } else if (data == '') {
-        res.send({
-          code: 400,
-          msg: '用户名或者密码出错！'
-        });
-      } else {
-        let user_id = data[0].id;
-        let result = await getUserInfo(user_id);
-        data[0].user_info = result[0];
-        res.send({
-          code: 200,
-          msg: '登录成功',
-          data: data[0]
-        });
-      }
-    }
-    dbConfig.sqlConnect(sql, sqlArr, callBack);
-  }
-}
-
-/**
  * 修改资料
  * @param req
  * @param res
  * @returns {Promise<void>}
  */
 let editUserInfo = async (req, res) => {
-  let {user_id, username, age, sex, job, path, birthday} = req.body;
-  let result = await setUserName(user_id, username);
-  console.log(result);
-  if (result) {
-    let data = await setUserInfo(user_id, username, age, sex, job, path, birthday);
-    console.log(data);
-    if (data.length) {
-      res.send({
-        code: 200,
-        data: data[0]
-      })
-    } else {
-      res.send({
-        code: 400,
-        msg: '修改失败'
-      })
-    }
+  let {user_id, username, password, avatar, age, sex, job, path, birthday, email, phone} = req.body;
+  let data = await setUserInfo(user_id, username, password, avatar, age, sex, job, path, birthday, email, phone);
+  if (data.length) {
+    res.send({
+      code: 200,
+      data: data[0]
+    })
   } else {
     res.send({
       code: 400,
@@ -417,6 +274,20 @@ let editUserInfo = async (req, res) => {
     })
   }
 }
+
+/**
+ * 修改用户名称
+ * @param user_id
+ * @param username
+ * @returns {Promise<boolean>}
+ */
+// mysql_affected_rows() 函数返回前一次 MySQL 操作所影响的记录行数。
+// let setUserName = async (user_id, username) => {
+//   let sql = `update user set username=? where id=?`;
+//   let sqlArr = [username, user_id];
+//   let res = await dbConfig.SySqlConnect(sql, sqlArr);
+//   return res.affectedRows == 1;
+// }
 
 /**
  * 检查用户密码
@@ -483,28 +354,101 @@ let setPassword = async (req, res) => {
   }
 }
 
+
+
 /**
- * 绑定用户邮箱接口
+ * 登录
  * @param req
  * @param res
- * @returns {Promise<void>}
  */
-let bindEmail = async (req, res) => {
-  let {user_id, email} = req.query;
-  let sql = `update user set email=? where id=?`;
-  let sqlArr = [email, user_id];
-  let result = await dbConfig.SySqlConnect(sql, sqlArr);
-  console.log(result);
-  if (result.affectedRows == 1) {
-    res.send({
-      code: 200,
-      msg: '邮箱绑定成功'
-    })
+//用户名、手机号登录,手机号+密码，邮箱+密码，用户名+密码
+let login = (req, res) => {
+  let username = req.body.username,
+      password = req.body.password;
+  let phone = /^1\d{10}$/;
+  let email = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
+  if (phone.test(username)) {
+    let sql = 'select * from user_info where phone=? and password=? or username=? and password=?';
+    let sqlArr = [username, password, username, password];
+    let callBack = async (err, data) => {
+      console.log(data);
+      if (err) {
+        console.log(err);
+        res.send({
+          code: 400,
+          msg: '出错了'
+        });
+      } else if (data == '') {
+        res.send({
+          code: 400,
+          msg: '用户名或者密码出错！'
+        });
+      } else {
+        let user_id = data[0].id;
+        let result = await getUserInfo(user_id);
+        data[0].user_info = result[0];
+        res.send({
+          code: 200,
+          msg: '登录成功',
+          data: data[0]
+        });
+      }
+    }
+    dbConfig.sqlConnect(sql, sqlArr, callBack);
+  } else if (email.test(username)) {
+    let sql = `select * from user_info where email=? and password=?`;
+    let sqlArr = [username, password];
+    let callBack = async (err, data) => {
+      if (err) {
+        console.log(err);
+        res.send({
+          code: 400,
+          msg: '出错了'
+        });
+      } else if (data == '') {
+        res.send({
+          code: 400,
+          msg: '用户名或者密码出错！'
+        });
+      } else {
+        let user_id = data[0].id;
+        let result = await getUserInfo(user_id);
+        data[0].user_info = result[0];
+        res.send({
+          code: 200,
+          msg: '登录成功',
+          data: data[0]
+        });
+      }
+    }
+    dbConfig.sqlConnect(sql, sqlArr, callBack);
   } else {
-    res.send({
-      code: 400,
-      msg: '邮箱绑定失败'
-    })
+    let sql = `select * from user_info where username=? and password=?`;
+    let sqlArr = [username, password];
+    let callBack = async (err, data) => {
+      if (err) {
+        console.log(err);
+        res.send({
+          code: 400,
+          msg: '出错了'
+        });
+      } else if (data == '') {
+        res.send({
+          code: 400,
+          msg: '用户名或者密码出错！'
+        });
+      } else {
+        let user_id = data[0].id;
+        let result = await getUserInfo(user_id);
+        data[0].user_info = result[0];
+        res.send({
+          code: 200,
+          msg: '登录成功',
+          data: data[0]
+        });
+      }
+    }
+    dbConfig.sqlConnect(sql, sqlArr, callBack);
   }
 }
 
