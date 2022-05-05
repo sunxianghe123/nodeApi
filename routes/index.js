8395c5d2d223c92983db15e2ed736653b775e5a0
sunxianghe123 <924909613@qq.com>
2022-05-05 17:21:34 +0800

    feat: 删除cate文件

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var cate = require('../controllers/cateController');
-
-/* GET home page. */
-router.get('/', cate.getCate);
-router.get('/getPostCate', cate.getPostCate);
 
 
 
